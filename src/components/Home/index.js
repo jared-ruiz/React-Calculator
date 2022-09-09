c7ba6536922f9c6a3c2c3fb3fc7425375e7cac2b
Jared Ruiz <jared707@gmail.com>
2022-09-09 04:59:01 -0500

    Added some comments, cleaned up index.css a bit, and added more styling to the welcome page.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-//I want to make a simple introduction page where the user clicks a button and it navigates
-//them to the calculation page
+//This page will simply serve as a very minimalistic home page to just get the user to engage
+//with the page prior to using the calculator. Ideally I just have the application start
+//with the calculator, but I wanted to practice routing again.
 
 const Home = () => {
     return(
         <div className='home-styling'>
             <div className='greeting'>
-                <h1>Welcome to My Custom Calculator</h1>
+                <h1>Welcome to My <span id='underline-intro'>Custom</span> Calculator!</h1>
                 <h2>Click Below To Begin!</h2>
                 <Link to='/calculator'>
                     <button id='start-button'>Start</button>
                 </Link>
-                
             </div>
         </div>
     )
