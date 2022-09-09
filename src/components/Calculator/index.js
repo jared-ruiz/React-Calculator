023930a49f02ebe7fd17ef5576a35952fe3408c0
Jared Ruiz <jared707@gmail.com>
2022-09-09 05:32:10 -0500

    Added some useState tests to see if I can conceptually grasp what I need to do for this calculator. I believe I'm heading in the right direction, but execution will be everything.

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { useState } from "react";
+
+//so my current idea is to take the current state which would be 0 everytime this component
+//is rendered, and have buttons that would change the state depending on what is clicked
+//for the operators, I will have to brainstorm some more.
 
 const Calculator = () => {
+
+    //test stateChange
+    const [stateChange, setStateChange] = useState('0');
+
+    //test for using innerText to grab the number values for calculator
+    const testStateChange = (e) => {
+        if (e.target.innerText === 'Clear') {
+            setStateChange('0')
+        }
+        else {
+            setStateChange(e.target.innerText);
+        }
+    }
+
     return(
         <div className="calculator-styling">
-            <h1>Test</h1>
+            <h1>Calculator</h1>
+            <h2 id="output">{stateChange}</h2>
+            <button onClick={testStateChange} id='test-button'>Test</button>
+            <button onClick={testStateChange} id='test-button'>Test2</button>
+            <button onClick={testStateChange} id='test-button'>Clear</button>
         </div>
     )
 }
