6864c652b9470c40c2c21c29b5dd209e355e5dae
Jared Ruiz <jared707@gmail.com>
2022-09-09 19:14:11 -0500

    Testing out parseInt to simplify the calculations. secondNumber useState may be cut soon. Messing with buttonHandler to see if I can cut out the initial zero state we start with.

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -7,35 +7,40 @@ import { useState } from "react";
 
 const Calculator = () => {
 
-    //starting state is an empty string
-    const [stateChange, setStateChange] = useState('');
+    //starting state to simply zero to start with
+    const [stateChange, setStateChange] = useState('0');
     const [operatorChange, setOperatorChange] = useState('')
-    const [firstNumber, setFirstNumber] = useState(0);
-    const [secondNumber, setSecondNumber] = useState(0);
+    const [firstNumber, setFirstNumber] = useState('');
+    const [secondNumber, setSecondNumber] = useState('');
 
     //test for using innerText to grab the number values for calculator
     //adding the values to current state seems to get what I want
     //im not sure if itll play out how I want though going forward but this is just
     //brainstorming atm
 
-    const testStateChange = (e) => {
-        if (e.target.innerText === 'Cl') {
-            setStateChange('');
-            setFirstNumber(0);
-            setSecondNumber(0);
-            setOperatorChange('');
-        }
-        else {
-            setStateChange(stateChange + e.target.innerText);
-        }
+    const handleClear = (e) => {
+        setStateChange('0');
+        setFirstNumber('');
+        setSecondNumber('');
+        setOperatorChange('');
+    }
+
+    const handleButtonClicks = (e) => {
+        setStateChange(stateChange + e.target.value);
     }
 
     //Currently I want to clear the state whenever an operator is clicked so I can assign 
     //the next button presses to number 2
     const handleOperators = (e) => {
-        if (e.target.innerText === '+' || '-' || '*' || '/') {
-            setOperatorChange(e.target.innerText);
+        if (e.target.value === '+' || '-' || '*' || '/') {
+
+            //assign operator state
+            setOperatorChange(e.target.value);
+
+            //assign current state to first number
             setFirstNumber(stateChange);
+
+            //clear state for second number
             setStateChange('');
         }
     }
@@ -44,11 +49,20 @@ const Calculator = () => {
     //ill see if I can assign state to number 2, do the math THEN update state again all in
     //one go
     const getResult = () => {
-        setStateChange(stateChange);
-        console.log(stateChange);
-        console.log(operatorChange);
-        console.log(firstNumber);
-        console.log(secondNumber);
+        
+        //assign secondNumber with newest state
+        setSecondNumber(stateChange);
+
+        //calculate based on which operator was selected
+        if (operatorChange === '+') {
+            var num1 = parseInt(firstNumber)
+            var num2 = parseInt(stateChange);
+            var result = (num1 + num2);
+            console.log(result);
+            setStateChange(result);
+        }
+
+        console.log(stateChange, firstNumber);
     }
 
     return(
@@ -56,21 +70,21 @@ const Calculator = () => {
             <h1>Calculator</h1>
             <h2 id="output">{stateChange}</h2>
             <div className="calculator-digits">
-                <button onClick={testStateChange} id='test-button'>1</button>
-                <button onClick={testStateChange} id='test-button'>2</button>
-                <button onClick={testStateChange} id='test-button'>3</button>
-                <button onClick={testStateChange} id='test-button'>4</button>
-                <button onClick={testStateChange} id='test-button'>5</button>
-                <button onClick={testStateChange} id='test-button'>6</button>
-                <button onClick={testStateChange} id='test-button'>7</button>
-                <button onClick={testStateChange} id='test-button'>8</button>
-                <button onClick={testStateChange} id='test-button'>9</button>
-                <button onClick={handleOperators} id='test-button'>+</button>
-                <button onClick={handleOperators} id='test-button'>-</button>
-                <button onClick={handleOperators} id='test-button'>/</button>
-                <button onClick={handleOperators} id='test-button'>*</button>
-                <button onClick={getResult} id='test-button'>=</button>
-                <button onClick={testStateChange} id='test-button'>Cl</button>
+                <button onClick={handleButtonClicks} id='test-button' value='1'>1</button>
+                <button onClick={handleButtonClicks} id='test-button' value='2'>2</button>
+                <button onClick={handleButtonClicks} id='test-button' value='3'>3</button>
+                <button onClick={handleButtonClicks} id='test-button' value='4'>4</button>
+                <button onClick={handleButtonClicks} id='test-button' value='5'>5</button>
+                <button onClick={handleButtonClicks} id='test-button' value='6'>6</button>
+                <button onClick={handleButtonClicks} id='test-button' value='7'>7</button>
+                <button onClick={handleButtonClicks} id='test-button' value='8'>8</button>
+                <button onClick={handleButtonClicks} id='test-button' value='9'>9</button>
+                <button onClick={handleOperators} id='test-button' value='+'>+</button>
+                <button onClick={handleOperators} id='test-button' value='-'>-</button>
+                <button onClick={handleOperators} id='test-button' value='/'>/</button>
+                <button onClick={handleOperators} id='test-button' value='*'>*</button>
+                <button onClick={getResult} id='test-button' value='='>=</button>
+                <button onClick={handleClear} id='test-button'>Cl</button>
             </div>
         </div>
     )
