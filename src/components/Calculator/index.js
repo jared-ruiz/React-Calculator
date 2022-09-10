c98b63c7c92529926df359668bf19937bab79890
Jared Ruiz <jared707@gmail.com>
2022-09-09 19:57:10 -0500

    Updated css, changed styling classes in calculator component. Additional index.js updates to remove random thought comments during early development.

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,36 +1,27 @@
 import React from "react";
 import { useState } from "react";
 
-//so my current idea is to take the current state which would be 0 everytime this component
-//is rendered, and have buttons that would change the state depending on what is clicked
-//for the operators, I will have to brainstorm some more.
-
 const Calculator = () => {
 
-    //starting state to simply zero to start with
+    //starting state
     const [stateChange, setStateChange] = useState('0');
     const [operatorChange, setOperatorChange] = useState('')
     const [firstNumber, setFirstNumber] = useState('');
-    const [secondNumber, setSecondNumber] = useState('');
 
-    //test for using innerText to grab the number values for calculator
-    //adding the values to current state seems to get what I want
-    //im not sure if itll play out how I want though going forward but this is just
-    //brainstorming atm
-
+    //reset state values
     const handleClear = (e) => {
         setStateChange('0');
         setFirstNumber('');
-        setSecondNumber('');
         setOperatorChange('');
     }
 
     const handleButtonClicks = (e) => {
-        setStateChange(stateChange + e.target.value);
+
+        //allows for initial state to remove '0' upon multiple button presses
+        //more convienient than having the button grid bounce around from empty state value
+        setStateChange(parseFloat(stateChange) + e.target.value);
     }
 
-    //Currently I want to clear the state whenever an operator is clicked so I can assign 
-    //the next button presses to number 2
     const handleOperators = (e) => {
         if (e.target.value === '+' || '-' || '*' || '/') {
 
@@ -41,50 +32,76 @@ const Calculator = () => {
             setFirstNumber(stateChange);
 
             //clear state for second number
-            setStateChange('');
+            setStateChange('0');
         }
     }
 
-    //assign result to current state
-    //ill see if I can assign state to number 2, do the math THEN update state again all in
-    //one go
     const getResult = () => {
-        
-        //assign secondNumber with newest state
-        setSecondNumber(stateChange);
 
-        //calculate based on which operator was selected
+        //assign state values as variables for later math, parseInt for clean output
+        var num1 = parseInt(firstNumber)
+        var num2 = parseInt(stateChange);
+
+        //check if the user used digits, else return error
+        if (!firstNumber || !stateChange) {
+            return setStateChange('No Digits Were Detected!');
+        }
+
+        //calculate addition operator
         if (operatorChange === '+') {
-            var num1 = parseInt(firstNumber)
-            var num2 = parseInt(stateChange);
+            
             var result = (num1 + num2);
-            console.log(result);
+            
+            //set state to result
             setStateChange(result);
         }
+        //calculate subtraction operator
+        else if (operatorChange === '-') {
+            
+            var result = (num1 - num2);
 
-        console.log(stateChange, firstNumber);
+            //set state to result
+            setStateChange(result);
+        }
+        //calculate division operator
+        else if (operatorChange === '/') {
+
+            var result = (num1 / num2);
+
+            //set state to result
+            setStateChange(result);
+        }
+        //calculate multiplication operator
+        else {
+            var result = (num1 * num2);
+
+            //set state to result
+            setStateChange(result);
+        }
     }
 
+    //output of state value via h2 element
     return(
         <div className="calculator-styling">
             <h1>Calculator</h1>
             <h2 id="output">{stateChange}</h2>
             <div className="calculator-digits">
-                <button onClick={handleButtonClicks} id='test-button' value='1'>1</button>
-                <button onClick={handleButtonClicks} id='test-button' value='2'>2</button>
-                <button onClick={handleButtonClicks} id='test-button' value='3'>3</button>
-                <button onClick={handleButtonClicks} id='test-button' value='4'>4</button>
-                <button onClick={handleButtonClicks} id='test-button' value='5'>5</button>
-                <button onClick={handleButtonClicks} id='test-button' value='6'>6</button>
-                <button onClick={handleButtonClicks} id='test-button' value='7'>7</button>
-                <button onClick={handleButtonClicks} id='test-button' value='8'>8</button>
-                <button onClick={handleButtonClicks} id='test-button' value='9'>9</button>
-                <button onClick={handleOperators} id='test-button' value='+'>+</button>
-                <button onClick={handleOperators} id='test-button' value='-'>-</button>
-                <button onClick={handleOperators} id='test-button' value='/'>/</button>
-                <button onClick={handleOperators} id='test-button' value='*'>*</button>
-                <button onClick={getResult} id='test-button' value='='>=</button>
-                <button onClick={handleClear} id='test-button'>Cl</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='1'>1</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='2'>2</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='3'>3</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='4'>4</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='5'>5</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='6'>6</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='7'>7</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='8'>8</button>
+                <button onClick={handleButtonClicks} className='button-styling' value='9'>9</button>
+                <button onClick={handleOperators} className='button-styling' value='+'>+</button>
+                <button onClick={handleOperators} className='button-styling' value='-'>-</button>
+                <button onClick={handleOperators} className='button-styling' value='/'>/</button>
+                <button onClick={handleOperators} className='button-styling' value='*'>*</button>
+                <button onClick={getResult} className='button-styling' value='=' id="equal-button">=</button>
+                <button onClick={handleClear} className='button-styling' id="clear-button">Clr</button>
+                
             </div>
         </div>
     )
